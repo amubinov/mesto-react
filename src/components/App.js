b9e9ba7ac5cf2825bdd8ddb45e2ba333c559e8be
alex <a.m.mubinov@yandex.ru>
2023-04-13 19:05:47 +0500

    fixed after first review

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { CurrentUserContext } from '../context/CurrentUserContext';
+
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
@@ -8,27 +9,29 @@ import EditAvatarPopup from './EditAvatarPopup';
 import AddPlacePopup from './AddPlacePopup';
 import ImagePopup from './ImagePopup';
 import ConfirmDeleteCardPopup from './ConfirmDeleteCardPopup';
+
 import '../App.css';
 import api from "../utils/api.js";
 
 function App() {
 
-  const [currentUser, setCurrentUser] = React.useState({});
-  const [cards, setCards] = React.useState([]);
-  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
-  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
-  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
-  const [isConfirmDeleteCardPopupOpen, setIsConfirmDeleteCardPopupOpen] = React.useState(false);
-  const [selectedCard, setSelectedCard] = React.useState({});
-  const [deleteCard, setDeleteCard] = React.useState({});
+  const [currentUser, setCurrentUser] = useState({});
+  const [cards, setCards] = useState([]);
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
+  const [isConfirmDeleteCardPopupOpen, setIsConfirmDeleteCardPopupOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState({});
+  const [deleteCard, setDeleteCard] = useState({});
 
-  React.useEffect(() => {
+  useEffect(() => {
     Promise.all([api.getCurrentUser(), api.getCards()])
-      .then(([userData, cardsData]) => {
-        setCurrentUser(userData);
-        setCards(cardsData);
-      })
-  }, [])
+        .then(([userData, cardsData]) => {
+          setCurrentUser(userData);
+          setCards(cardsData);
+        })
+        .catch((error) => console.log(error));
+  }, []);
 
   function handleEditProfileClick() {
     setIsEditProfilePopupOpen(!isEditProfilePopupOpen);
@@ -61,37 +64,47 @@ function App() {
 
   function handleCardLike(card) {
     const isLiked = card.likes.some(i => i._id === currentUser._id);
-    api.changeLikeCardStatus(card._id, !isLiked).then((newCard) => {
-      setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
-    });
+    api.changeLikeCardStatus(card._id, !isLiked)
+        .then((newCard) => {
+          setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
+        })
+        .catch((error) => console.log(error));
   }
 
   function handleCardDelete(card) {
-    api.deleteCard(card._id).then(() => {
-      setCards((state) => state.filter((c) => c._id !== card._id));
-      closeAllPopups();
-    })
+    api.deleteCard(card._id)
+        .then(() => {
+          setCards((state) => state.filter((c) => c._id !== card._id));
+          closeAllPopups();
+        })
+        .catch((error) => console.log(error));
   }
 
   function handleUpdateUser(name, about) {
-    api.changeUserData(name, about).then(userData => {
-      setCurrentUser(userData);
-      closeAllPopups();
-    })
+    api.changeUserData(name, about)
+        .then(userData => {
+          setCurrentUser(userData);
+          closeAllPopups();
+        })
+        .catch((error) => console.log(error));
   }
 
   function handleUpdateAvatar(avatarLink) {
-    api.changeAvatar(avatarLink).then(userData => {
-      setCurrentUser(userData);
-      closeAllPopups();
-    })
+    api.changeAvatar(avatarLink)
+        .then(userData => {
+          setCurrentUser(userData);
+          closeAllPopups();
+        })
+        .catch((error) => console.log(error));
   }
 
   function handleAddPlaceSubmit(name, link) {
-    api.addNewCard(name, link).then(cardData => {
-      setCards([cardData, ...cards]);
-      closeAllPopups();
-    })
+    api.addNewCard(name, link)
+        .then(cardData => {
+          setCards([cardData, ...cards]);
+          closeAllPopups();
+        })
+        .catch((error) => console.log(error));
   }
 
   const mainProps = {
@@ -135,18 +148,18 @@ function App() {
   }
 
   return (
-    <div className="page">
-      <CurrentUserContext.Provider value={currentUser}>
-        <Header />
-        <Main {...mainProps} />
-        <Footer />
-        <EditProfilePopup {...editProfilePopupProps} />
-        <AddPlacePopup {...addPlacePopupProps} />
-        <EditAvatarPopup {...editAvatarPopupProps} />
-        <ConfirmDeleteCardPopup {...confirmDeleteCardPopupProps} />
-        <ImagePopup {...imagePopupProps} />
-      </CurrentUserContext.Provider>
-    </div>
+      <div className="page">
+        <CurrentUserContext.Provider value={currentUser}>
+          <Header />
+          <Main {...mainProps} />
+          <Footer />
+          <EditProfilePopup {...editProfilePopupProps} />
+          <AddPlacePopup {...addPlacePopupProps} />
+          <EditAvatarPopup {...editAvatarPopupProps} />
+          <ConfirmDeleteCardPopup {...confirmDeleteCardPopupProps} />
+          <ImagePopup {...imagePopupProps} />
+        </CurrentUserContext.Provider>
+      </div>
   );
 }
 
--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -3,42 +3,39 @@ import PopupWithForm from "./PopupWithForm";
 import { CurrentUserContext } from "../context/CurrentUserContext";
 
 function EditProfilePopup(props) {
-
   const currentUser = React.useContext(CurrentUserContext);
-  const [profileState, setProfileState] = React.useState({ name: '', about: '' });
+  const [formData, setFormData] = React.useState({ name: currentUser.name, about: currentUser.about });
 
   React.useEffect(() => {
-    setProfileState({ name: currentUser.name, about: currentUser.about })
-  }, [props.isOpen]);
+    setFormData({ name: currentUser.name, about: currentUser.about })
+  }, [props.isOpen, currentUser]);
 
   function handleChange(e) {
-    setProfileState({ ...profileState, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    props.onUpdateUser(profileState.name, profileState.about);
-  }
-
-  const popupWithFormProps = {
-    name: 'edit',
-    isOpen: props.isOpen,
-    title: 'Редактировать профиль',
-    children: <>
-      <input onChange={handleChange} value={profileState.name || ''} type="text" name="name" placeholder="Ваше имя" id="name-input"
-        className="popup__input popup__input_type_name" minLength="2" maxLength="40" required autoComplete="off" />
-      <span className="popup__error name-input-error"></span>
-      <input onChange={handleChange} value={profileState.about || ''} type="text" name="about" placeholder="Ваша деятельность" id="activity-input"
-        className="popup__input popup__input_type_activity" minLength="2" maxLength="200" required autoComplete="off" />
-      <span className="popup__error activity-input-error"></span>
-    </>,
-    submitButtonTextContent: 'Сохранить',
-    onClosePopup: props.onClose,
-    onSubmit: handleSubmit
+    props.onUpdateUser(formData.name, formData.about);
   }
 
   return (
-    <PopupWithForm {...popupWithFormProps} />
+      <PopupWithForm
+          name="edit"
+          isOpen={props.isOpen}
+          title="Редактировать профиль"
+          submitButtonTextContent='Сохранить'
+          onClosePopup={props.onClose}
+          onSubmit={handleSubmit}
+      >
+        <input onChange={handleChange} value={formData.name || ''} type="text" name="name" placeholder="Ваше имя" id="name-input"
+               className="popup__input popup__input_type_name" minLength="2" maxLength="40" required autoComplete="off" />
+        <span className="popup__error name-input-error"></span>
+        <input onChange={handleChange} value={formData.about || ''} type="text" name="about" placeholder="Ваша деятельность" id="activity-input"
+               className="popup__input popup__input_type_activity" minLength="2" maxLength="200" required autoComplete="off" />
+        <span className="popup__error activity-input-error"></span>
+      </PopupWithForm>
   )
 }
 
